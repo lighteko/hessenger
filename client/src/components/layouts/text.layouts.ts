8c8656c549913e747b0ab1c0d117b2df1943f000
lighteko <dev.lighteko@gmail.com>
2025-01-11 23:49:13 -0500

    .

--- client/src/components/layouts/text.layouts.ts
+++ client/src/components/layouts/text.layouts.ts
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
-export const TitleLayout = styled.p`
-  font-weight: bold;
-  font-size: 1em;
+export const TextLayout = styled.p<{
+  margin: number;
+  size: number;
+  weight: string | null;
+}>`
+  font-weight: ${(props) => props.weight || "400"};
+  font-size: ${(props) => props.size}em;
+  margin: ${(props) => props.margin}em 0;
 `;
